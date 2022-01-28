80db151e5b1aafaf8a83084586e083a520df06ce
leresteux <66775304+lensemble@users.noreply.github.com>
2022-01-28 19:14:17 +0100

    Update scp-V1.ino

--- arduino/scp-V1/scp-V1.ino
+++ arduino/scp-V1/scp-V1.ino
@@ -2,24 +2,33 @@
 #include <Servo.h>
 
 byte startAngle = 90;
-bool directionAngle[4];
+bool directionAngle[4]={1,1,1,1};
 int pos[4];
+int redpin = 9;
+int bluepin = 10;
+int greenpin = 11;
+int whitePower = 200;
+int maxPower = 240;
+int miniPower = 100;
 
 Servo servo[4];
 
 void setup() {
-  
-  for (byte i = 0; i < 4; i++) {
-    servo[i].attach(i);
-    pos[i] = startAngle;
-    servo[i].write(pos[i]);
-  }
+  pos[0]=startAngle;
+  pinMode(redpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  servo[0].attach(3);
+  pos[0] = startAngle;
+  servo[0].write(pos[0]);
 }
 
+
 void loop() {
-  for (byte i = 0; i < 4; i++) {
-    move(i);
-  }
+
+ // move(0);
+  ledRVB();
+
 }
 
 void move(byte whatServoInfo) {
@@ -32,5 +41,18 @@ void move(byte whatServoInfo) {
     pos[whatServoInfo]--;
   }
   servo[whatServoInfo].write(pos[whatServoInfo]);
+}
 
+void ledRVB() {
+
+  whitePower = whitePower + random(-1, 3);
+  if (whitePower < miniPower) {
+    whitePower = miniPower;
+  }
+  if (whitePower > maxPower) {
+    whitePower = maxPower;
+  }
+  analogWrite(redpin, whitePower);
+  analogWrite(bluepin, whitePower);
+  analogWrite(greenpin, whitePower);
 }
