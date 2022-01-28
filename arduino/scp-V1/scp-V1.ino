cfa251fe752a8e51461214a7bd71f4d0a2271e7e
julien leresteux <leresteuxj@gmail.com>
2022-01-29 00:13:16 +0100

    Update scp-V1.ino

--- arduino/scp-V1/scp-V1.ino
+++ arduino/scp-V1/scp-V1.ino
@@ -1,36 +1,89 @@
 //https://www.tutorialspoint.com/arduino/arduino_multi_dimensional_arrays.htm
 #include <Servo.h>
 
-byte startAngle = 90;
-bool directionAngle[4];
-int pos[4];
+const byte startAngle = 90;
+const byte nbreServo = 6;
+const byte posMini = 10;
+const byte posMax = 170;
+const byte nbrededegreMax = 20;
+const byte nbredevibrationMax = 20;
 
-Servo servo[4];
+bool directionAngle[nbreServo];
+int pos[nbreServo];
+byte nbrededegre[nbreServo] = {1,1,1,1,1,1};
+byte colere[nbreServo] = { };
+Servo servo[nbreServo];
 
 void setup() {
-  
-  for (byte i = 0; i < 4; i++) {
-    servo[i].attach(i);
+  servo[0].attach(3);
+  servo[1].attach(5);
+  servo[2].attach(6);
+  servo[3].attach(9);
+  servo[4].attach(10);
+  servo[5].attach(11);
+
+  Serial.begin(9600);
+
+  for (byte i = 0; i < nbreServo; i++) {
+
     pos[i] = startAngle;
-    servo[i].write(pos[i]);
+    servo[i].write(90);
   }
 }
 
 void loop() {
-  for (byte i = 0; i < 4; i++) {
-    move(i);
+  if (random(10)==0) {
+    colere[random(nbreServo)] = 1;
+  }
+  for (byte i = 0; i < nbreServo; i++) {
+ 
+    moveMode(i);
+    safelimites(i);
+    servo[i].write(pos[i]);
+
+    Serial.print("Servo ");
+    Serial.print(i);
+    Serial.print(" - var en degre = ");
+    Serial.println(nbrededegre[i]);
   }
 }
 
-void move(byte whatServoInfo) {
-  if (pos[whatServoInfo] < 10 or pos[whatServoInfo] > 170) {
+
+void moveMode(byte whatServoInfo) {
+  // on change de nbrededegre
+  if (pos[whatServoInfo] <= posMini or pos[whatServoInfo] >= posMax) {
     directionAngle[whatServoInfo] = !directionAngle[whatServoInfo];
   }
-  if (directionAngle[whatServoInfo]) {
-    pos[whatServoInfo]++;
-  } else {
-    pos[whatServoInfo]--;
+  if (colere[whatServoInfo] != 0) {
+    nbrededegre[whatServoInfo] = nbrededegre[whatServoInfo] + random(-2, 5);
   }
-  servo[whatServoInfo].write(pos[whatServoInfo]);
 
+  // remise au minimun la nbrededegre de tous si un servo arrive au max et coleremode off
+  if ( nbrededegre[whatServoInfo] > nbrededegreMax) {
+    for (byte j = 0; j < nbreServo; j++) {
+      nbrededegre[j] = 1;
+      colere[j] = 0;
+    }
+  }
+}
+
+void safelimites(byte whatServoInfo) {
+
+  // on modifie la direction en bout de limite
+
+  if (directionAngle[whatServoInfo]) {
+    pos[whatServoInfo] = pos[whatServoInfo] + nbrededegre[whatServoInfo];
+  } else {
+    pos[whatServoInfo] = pos[whatServoInfo] - nbrededegre[whatServoInfo];
+  }
+  // on reste dans les limite d'angle
+  if (pos[whatServoInfo] > posMax) {
+    pos[whatServoInfo] = posMax;
+  }
+  if (pos[whatServoInfo] < posMini) {
+    pos[whatServoInfo] = posMini;
+  }
+  if (nbrededegre[whatServoInfo] < 1) {
+    nbrededegre[whatServoInfo] = 1;
+  }
 }
